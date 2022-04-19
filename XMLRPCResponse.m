ebeda90d413a4c9ed83295f44bb003bfb7636317
Troy Stephens <troy@coherencelabs.com>
2022-04-19 16:11:18 -0500

    For TypeMetal: Added defensive control-character filtering to XMLRPCResponse.
    WordPress post/page content may contain spurious control characters that XML parsing chokes on.

--- XMLRPCResponse.m
+++ XMLRPCResponse.m
@@ -1,6 +1,40 @@
 #import "XMLRPCResponse.h"
 #import "XMLRPCEventBasedParser.h"
 
+#define FILTER_BUFFER_SIZE  1024
+
+static NSData *dataByRemovingNonXMLControlCharacters(NSData *sourceData)
+{
+    unsigned char *buffer = (unsigned char *)malloc(FILTER_BUFFER_SIZE * sizeof(unsigned char));
+    if (buffer == NULL) {
+        return sourceData; // No choice but to bail out, but in practice we shouldn't hit this for such a small buffer allocation.
+    }
+    NSUInteger sourceDataLength = [sourceData length];
+    NSMutableData *result = [[NSMutableData alloc] initWithCapacity:sourceDataLength];
+    NSUInteger sourceDataOffset = 0;
+    while (sourceDataOffset < sourceDataLength) {
+        NSUInteger thisPageSourceLength = MIN(FILTER_BUFFER_SIZE, sourceDataLength - sourceDataOffset);
+        [sourceData getBytes:buffer range:NSMakeRange(sourceDataOffset, thisPageSourceLength)];
+        unsigned char *source = buffer;
+        unsigned char *sourceEnd = buffer + thisPageSourceLength;
+        unsigned char *dest = source;
+        while (source < sourceEnd) {
+            unsigned char c = *source;
+            if (c <= 27 && (c != '\t' && c != '\n' && c != '\r')) { // Filter control characters XML disallows. (The sender should probably have escaped these.)
+                ++source;
+            } else {
+                *dest++ = *source++;
+            }
+        }
+        NSUInteger thisPageResultLength = dest - buffer;
+        [result appendBytes:buffer length:thisPageResultLength];
+        sourceDataOffset += thisPageSourceLength;
+    }
+    
+    free(buffer);
+    return [result autorelease];
+}
+
 @implementation XMLRPCResponse
 
 - (id)initWithData: (NSData *)data {
@@ -8,9 +42,12 @@
         return nil;
     }
 
+    // WordPress post/page content may contain spurious control characters that XML parsing chokes on.  Filter them out.
+    NSData *filteredData = dataByRemovingNonXMLControlCharacters(data);
+
     self = [super init];
     if (self) {
-        XMLRPCEventBasedParser *parser = [[XMLRPCEventBasedParser alloc] initWithData: data];
+        XMLRPCEventBasedParser *parser = [[XMLRPCEventBasedParser alloc] initWithData: filteredData];
         
         if (!parser) {
 #if ! __has_feature(objc_arc)
@@ -19,7 +56,7 @@
             return nil;
         }
     
-        myBody = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+        myBody = [[NSString alloc] initWithData: filteredData encoding: NSUTF8StringEncoding];
         myObject = [parser parse];
 #if ! __has_feature(objc_arc)
         [myObject retain];
